68642f837a509bd54da1732f9f3c82da00f04d87
Hans-Christian Fjeldberg <hans-christian@fjeldbergconsulting.no>
2013-03-03 22:41:15 +0100

    Adding a simple node.js implementation

---
+++ node_server/chat.js
@@ -0,0 +1,49 @@
+// Holds a list of connections. {name: "actual name"}
+var connections = [];
+
+var WebSocketServer = require('ws').Server;
+var posix = require('posix');
+
+console.log("ulimit -n: " + posix.getrlimit("nofile").hard);
+
+var wss = new WebSocketServer({port: 10100});
+
+console.log("started websocket server, listening on port 10100");
+
+wss.on('connection', function(ws) {
+
+    ws.on('message', function(message) {
+
+    	var json = JSON.parse(message);
+
+    	var eventName = json.event;
+
+    	var data = json.data;
+
+    	console.log("received event: " +eventName);
+
+    	if (eventName === "users") {
+    		ws.send('{"event": "users", "data": [{"name": "etz"}]}');
+    	}
+
+    	else if (eventName === "new_message") {
+    		connections.forEach(function(connection) {
+    			connection.send(message);
+    		})
+    	}
+
+    	else if (eventName === "me") {
+
+    	}
+
+    	else {
+    		console.log("received unknown event");
+    	}
+
+    });
+
+	connections.push(ws);
+
+    ws.send('{"event": "welcome", "data": {"text": "Welcome"}}');
+
+});
